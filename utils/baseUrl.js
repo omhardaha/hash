9017a04a3e802db1804f65ec16313d2e6b087a32
omhardaha <88886428+omhardaha@users.noreply.github.com>
2022-10-06 00:21:11 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:4000" : "https://hash-sable.vercel.app/";
+const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:4000" : "https://social-media-omhardaha.herokuapp.com";
 
 export default baseUrl;
