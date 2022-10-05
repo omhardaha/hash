597a5354e32db65c6c0a6999080acb64d9d08977
omhardaha <88886428+omhardaha@users.noreply.github.com>
2022-10-06 00:29:21 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:4000" : "https://social-media-omhardaha.herokuapp.com";
+const baseUrl = process.env.NODE_ENV !== 'production' ? "http://localhost:4000" : "https://hash-social-med.herokuapp.com";
 
 export default baseUrl;
